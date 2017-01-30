e59c448022725cb45771f7226e277f71f7eaa9e2
mate2 <mate2bob@gmail.com>
2017-01-30 08:12:23 -0500

    add manager app

---
+++ manager/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
+import reducers from './reducers';
+import Router from './Router';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
